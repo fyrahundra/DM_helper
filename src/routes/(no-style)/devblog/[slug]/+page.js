5708947a16a2bf3f382b0003b6c3c7ded52185f1
Jonathan Karlgren <jonkar11@elevad.umea.se>
2025-04-09 14:05:13 +0200

    message

--- src/routes/(no-style)/devblog/[slug]/+page.js
+++ src/routes/(no-style)/devblog/[slug]/+page.js
@@ -5,7 +5,11 @@ export const prerender = false;
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
-  const post = await import("../"+params.slug+ ".svx");
+  //const post = await import("../"+params.slug+ ".svx");
+
+  const posts = import.meta.glob('../*.svx');
+  const match = posts[`../${params.slug}.svx`];
+  const post = await match();
 
   const { title, date } = post.metadata;
   const content = post.default;
